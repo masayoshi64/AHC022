2068bb06100c552029cf9d3fd02ebe111383e4ca
masayoshi64 <sho32003200@gmail.com>
2023-08-12 20:07:47 +0900

    推定パートを追加

--- main.cpp
+++ main.cpp
@@ -8,7 +8,6 @@ using ll = long long;
 using ull = unsigned long long;
 using ld = long double;
 typedef pair<ll, ll> Pl;
-typedef pair<int, int> Pi;
 typedef vector<ll> vl;
 typedef vector<int> vi;
 typedef vector<char> vc;
@@ -307,6 +306,97 @@ long long xor64(long long range) {
     return (x ^= x << 17) % range;
 }
 
+template <typename flow_t, typename cost_t>
+struct MinCostFlow {
+    const cost_t TINF;
+
+    struct edge {
+        int to;
+        flow_t cap;
+        cost_t cost;
+        int rev;
+        bool isrev;
+    };
+    vector<vector<edge> > graph;
+    vector<cost_t> potential, min_cost;
+    vector<int> prevv, preve;
+
+    MinCostFlow(int V) : TINF(numeric_limits<cost_t>::max()), graph(V) {}
+
+    void add_edge(int from, int to, flow_t cap, cost_t cost) {
+        graph[from].emplace_back(
+            (edge){to, cap, cost, (int)graph[to].size(), false});
+        graph[to].emplace_back(
+            (edge){from, 0, -cost, (int)graph[from].size() - 1, true});
+    }
+
+    cost_t min_cost_flow(int s, int t, flow_t f) {
+        int V = (int)graph.size();
+        cost_t ret = 0;
+        using Pi = pair<cost_t, int>;
+        priority_queue<Pi, vector<Pi>, greater<Pi> > que;
+        potential.assign(V, 0);
+        preve.assign(V, -1);
+        prevv.assign(V, -1);
+
+        while (f > 0) {
+            min_cost.assign(V, TINF);
+            que.emplace(0, s);
+            min_cost[s] = 0;
+            while (!que.empty()) {
+                Pi p = que.top();
+                que.pop();
+                if (min_cost[p.second] < p.first) continue;
+                for (int i = 0; i < graph[p.second].size(); i++) {
+                    edge &e = graph[p.second][i];
+                    cost_t nextCost = min_cost[p.second] + e.cost +
+                                      potential[p.second] - potential[e.to];
+                    if (e.cap > 0 && min_cost[e.to] > nextCost) {
+                        min_cost[e.to] = nextCost;
+                        prevv[e.to] = p.second, preve[e.to] = i;
+                        que.emplace(min_cost[e.to], e.to);
+                    }
+                }
+            }
+            if (min_cost[t] == TINF) return -1;
+            for (int v = 0; v < V; v++) potential[v] += min_cost[v];
+            flow_t addflow = f;
+            for (int v = t; v != s; v = prevv[v]) {
+                addflow = min(addflow, graph[prevv[v]][preve[v]].cap);
+            }
+            f -= addflow;
+            ret += addflow * potential[t];
+            for (int v = t; v != s; v = prevv[v]) {
+                edge &e = graph[prevv[v]][preve[v]];
+                e.cap -= addflow;
+                graph[v][e.rev].cap += addflow;
+            }
+        }
+        return ret;
+    }
+
+    void output() {
+        for (int i = 0; i < graph.size(); i++) {
+            for (auto &e : graph[i]) {
+                if (e.isrev) continue;
+                auto &rev_e = graph[e.to][e.rev];
+                cout << i << "->" << e.to << " (flow: " << rev_e.cap << "/"
+                     << rev_e.cap + e.cap << ")" << endl;
+            }
+        }
+    }
+};
+
+const int min_tmp = 20;
+const int max_tmp = 1000;
+const int max_measurements = 10000;
+double measure(int i, int x, int y){
+    cout << i << ' ' << x << ' ' << y << endl;
+    double T;
+    cin >> T;
+    return T;
+}
+
 
 int main(int argc, char *argv[]) {
     cin.tie(0);
@@ -322,7 +412,15 @@ int main(int argc, char *argv[]) {
     }
 
     // 配置
+    vi exit_tmps(N);
     mat<int> P(L, vi(L, 0));
+    int dT = (max_tmp - min_tmp) / N;
+    rep(i, N) {
+        exit_tmps[i] = min_tmp + i * dT;
+    }
+    rep(i, N){
+        P[X[i]][Y[i]] = exit_tmps[i];
+    }
     rep(i, L) {
         rep(j, L){
             cout << P[i][j] << ' ';
@@ -331,11 +429,36 @@ int main(int argc, char *argv[]) {
     }
 
     //計測
+    int m = max_measurements / N;
+    vector<double> ave_tmps(N);
+    rep(i, N){
+        rep(j, m){
+            ave_tmps[i] += measure(i, 0, 0) / m;
+        }
+    }
     cout << -1 << ' ' << -1 << ' ' << -1 << endl;
 
     // 回答
+    MinCostFlow<int, double> mcf(N * 2 + 2);
+    rep(i, N){
+        mcf.add_edge(N * 2, i, 1, 0);
+        mcf.add_edge(N + i, N * 2 + 1, 1, 0);
+        rep(j, N){
+            double cost = mypow<double>(ave_tmps[i] - exit_tmps[j], 2);
+            mcf.add_edge(i, N + j, 1, cost);
+        }
+    }
+    mcf.min_cost_flow(N * 2, N * 2 + 1, N);
     vi E(N);
-    iota(all(E), 0);
+    rep(i, N){
+        for(auto& e : mcf.graph[i]){
+            if (e.isrev) continue;
+            auto &rev_e = mcf.graph[e.to][e.rev];
+            if(rev_e.cap == 1){
+                E[i] = e.to - N;
+            }
+        }
+    }
     rep(i, N){
         cout << E[i] << endl;
     }
